a1076c11aedaa45cf8be6caef1985f8bc19c4c80
goodplayer <meidomx@gmail.com>
2016-05-21 23:32:21 +0800

    update inventory data

---
+++ prepare/update/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+
+	"gopkg.in/jackc/pgx.v2"
+)
+
+func main() {
+	config := pgx.ConnPoolConfig{
+		ConnConfig: pgx.ConnConfig{
+			Host:     "127.0.0.1",
+			Port:     5432,
+			Database: "inventory",
+			User:     "inventoryuser",
+			Password: "inventoryuser",
+		},
+		MaxConnections: 20,
+	}
+
+	pool, err := pgx.NewConnPool(config)
+
+	if err != nil {
+		log.Fatalln("new conn pool error.", err)
+	}
+
+	_, err = pool.Exec("update item_inventory set quantity = 999999999999 where quantity < 999999999999;")
+	if err != nil {
+		log.Fatalln("update item_inventory error.", err)
+	}
+}
